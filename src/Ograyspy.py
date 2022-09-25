9506b23bf241d98ca0608155abb6755815ee8dfd
terrabela <maduar@vivaldi.net>
2022-09-25 14:29:56 -0300

    Improve spectra path definitions.

--- src/Ograyspy.py
+++ src/Ograyspy.py
@@ -1,3 +1,4 @@
+import platform
 from random import randrange
 from typing import List
 
@@ -16,10 +17,20 @@ class Ograyspy:
         self.odometer = 0
         self.time = 0
         # self.dir_list = DirectoryList()
-        self.curr_path = Path('../../../OwnDrive/Genie_Transfer')
-        # print(len(self.dir_list))
+        info_plat = platform.platform()
+        info_mach = platform.machine()
+        info_syst = platform.system()
+        info_node = platform.node()
+        print(info_plat + ' ' + info_mach + ' ' + info_syst + ' ' + info_node)
 
-        my_file = 'ogra_pic_f.pkl'
+        # Locate the general folder containing spectra in the current system
+        general_spectra_folder_name = 'Genie_Transfer'
+
+        interm_list = [i for i in Path.home().glob('*/' + general_spectra_folder_name)]
+        self.spectra_path = interm_list[0]
+        print(self.spectra_path)
+
+        my_file = 'ogra_pic_f_' + info_syst + '_' + info_node + '.pkl'
 
         if os.path.isfile(my_file):  # if file exists we have already pickled a list
             print('Load existing pickle file')
@@ -28,9 +39,9 @@ class Ograyspy:
         else:
             print('Create new pickle file')
             self.files_list = []
-            for i in self.curr_path.glob("**/*.[Cc][Hh][Nn]"):
+            for i in self.spectra_path.glob("**/*.[Cc][Hh][Nn]"):
                 self.files_list.append('/'.join(i.parts[5:]))
-            for i in self.curr_path.glob("**/*.[Ii][Ee][Cc]"):
+            for i in self.spectra_path.glob("**/*.[Ii][Ee][Cc]"):
                 self.files_list.append('/'.join(i.parts[5:]))
             with open(my_file, 'wb') as f:
                 pickle.dump(self.files_list, f)
@@ -46,16 +57,20 @@ if __name__ == '__main__':
     print("I'm a OGRaySPy (gamma-ray spectra analyzer!")
     print('No. spec files: ', my_ogra.n_files)
 
-    given_spec_name = "Filtros/2022/Cci/CCI1603-I.Chn"
+    # given_spec_name = "Filtros/2022/Cci/CCI1603-I.Chn"
+    # given_spec_name = "Si/SI2018/SI11318.Chn"
+    given_spec_name = "Eso_non_existe.Chn"
     if given_spec_name in my_ogra.files_list:
         print("Found!")
-        complete_spec_name = '../../../OwnDrive/Genie_Transfer' + '/' + given_spec_name
+        complete_spec_name = str(my_ogra.spectra_path) + '/' + given_spec_name
     else:
+        print("Not found... :-( ")
         my_ogra.choose_random_spectrum()
         print('Random spec index: ', my_ogra.a_spec_ind)
         a_spec_name: str = my_ogra.files_list[my_ogra.a_spec_ind]
         print('...and its name: ', a_spec_name)
-        complete_spec_name = '../../../OwnDrive/Genie_Transfer' + '/' + a_spec_name
+        complete_spec_name = str(my_ogra.spectra_path) + '/' + a_spec_name
+    print(complete_spec_name)
     a_spec = Spec(complete_spec_name)
     a_spec.total_analysis()
     # 2022-Jun-24 No momento esses são os que interessam:
